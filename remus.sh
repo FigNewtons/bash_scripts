6379c43e2c6ea12127c4cdd9722495ebefaba628
Daniel Gruszczynski <dangruszczynski@gmail.com>
2013-10-25 22:46:19 -0400

    Added user argument

--- remus.sh
+++ remus.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Renames music files in a given directory
+# Renames music files in a given directory based on extension
+# provided by the user (ex: bash remus.sh mp3)
 # Make sure to cd inside the target directory before running
 
+EXT=$1
 COUNTER=1
-for file in *.mp3  #Change to any extension
+for file in *$EXT  #Change to any extension
 do
 	#Removes substring preceding the last dash (-) in string 
 	new=$COUNTER."${file##*-}" 
 	mv "$file" "$new"
 	let COUNTER=$[$COUNTER + 1]
 done
-
 
 
 
